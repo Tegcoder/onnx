b54840e51e93adb5b1eaae665825bca9d980c509
chen UANUAN <tegcobyname@outlook.com>
2023-09-25 11:20:40 +0800

    去除多余代码，优化代码格式，并加入更多注释

--- MINST_LGYTest.py
+++ MINST_LGYTest.py
@@ -7,36 +7,49 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import time
 
-startTime = time.time()
-# 加载数据集
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+# 设置随机种子以确保结果可重复性
+torch.manual_seed(0)
+
+# 加载数据集并进行数据预处理
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+train_set = torchvision.datasets.MNIST(
+    root='./data', train=True, download=True, transform=transform)
+test_set = torchvision.datasets.MNIST(
+    root='./data', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_set, batch_size=512, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=512, shuffle=False)
 
 # 定义模型
+
+
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 256)
-        self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64, 10)
+        self.fc_layers = nn.Sequential(
+            nn.Linear(28 * 28, 256),  # 输入层到隐藏层1
+            nn.ReLU(),
+            nn.Linear(256, 64),      # 隐藏层1到隐藏层2
+            nn.ReLU(),
+            nn.Linear(64, 10)        # 隐藏层2到输出层
+        )
 
     def forward(self, x):
         x = x.view(-1, 28 * 28)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.fc_layers(x)
 
+
+# 检查GPU是否可用，如果可用则使用GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = SimpleNet().to(device)
 
 # 定义损失函数和优化器
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+# 计时器开始
+startTime = time.time()
 
 # 训练模型
 num_epochs = 10
@@ -52,7 +65,7 @@ for epoch in range(num_epochs):
 
     print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item()}")
 
-# 测试模型
+# 测试模型性能
 model.eval()
 with torch.no_grad():
     correct = 0
@@ -64,13 +77,22 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-    print(f"Accuracy: {100 * correct / total}%")
-torch.save(model, 'MINIST_model.pth')
+    accuracy = 100 * correct / total
+    print(f"Accuracy: {accuracy}%")
+
+# 保存模型
+# torch.save(model, 'MNIST_model.pth')
+model_filename = f'MNIST_model_{time.strftime("%Y%m%d%H%M%S")}.pth'
+torch.save(model, model_filename)
+
+# 计时器结束
+endTime = time.time()
+print(f"代码执行时间：{endTime - startTime}秒")
 
 # 加载模型并显示部分测试集图像和预测结果
-modelx = torch.load('MINIST_model.pth')
+modelx = torch.load('MNIST_model.pth')
 modelx.eval()
-# 显示部分测试集图像和预测结果
+
 samples = 10
 fig, axes = plt.subplots(1, samples, figsize=(samples * 2, 2))
 for i in range(samples):
@@ -83,21 +105,3 @@ for i in range(samples):
     axes[i].set_title(f"Predicted: {predicted.item()}")
     axes[i].axis("off")
 plt.show()
-endTime = time.time()
-print(endTime-startTime)
-
-# def plot_image(img, label, name):
-#     fig = plt.figure()
-#     for i in range(6):
-#         plt.subplot(2, 3, i + 1)
-#         plt.tight_layout()
-#         plt.imshow(img[i][0] * 0.3081 + 0.1307, cmap='gray', interpolation='none')
-#         plt.title("{}: {}".format(name, label[i].item()))
-#         plt.xticks([])
-#         plt.yticks([])
-#     plt.show()
-#
-# image, label = next(iter(test_loader))
-# out = modelx(image.view(image.size(0), 28 * 28).to(device))
-# pred = out.argmax(dim=1)
-# plot_image(image, pred, 'test')
